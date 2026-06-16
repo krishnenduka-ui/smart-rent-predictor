b8b1fc54e8a42ec2311bf1e62be6a951ed0da98f
krishnendu <krishnendu.ka@gmail.com>
2026-06-16 19:22:00 +0530

    toast added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,6 +15,7 @@ createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <BrowserRouter>
         <App />
+        <Toaster position="top-right" />
       </BrowserRouter>
     </Provider>
     
